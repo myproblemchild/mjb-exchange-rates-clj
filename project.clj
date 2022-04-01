7e06e0cf07312a89c32870acce0fd0efc33b2787
t33ltd <t33ltd@t33ltd.com>
2022-03-31 23:30:11 -0700

    ...

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [
-      [org.clojure/clojure "1.10.3"],
-      [clj-http "3.12.3"]
-      [cheshire "5.10.2"] ;; to enable get http as json
-      [http-kit "2.5.3"]  ;; for web-server      
-      ]
+                 [org.clojure/clojure "1.10.3"],
+                 [clj-http "3.12.3"]
+                 [cheshire "5.10.2"] ;; to enable get http as json
+                 [http-kit "2.5.3"]  ;; for web-server
+                 ]
   :main ^:skip-aot google-sheets.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
